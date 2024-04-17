77944e9e41f90368fe4d6b785c5198e8251551fe
Sanjeev Shrestha <sanjeevsht123@gmail.com>
2024-04-17 15:30:14 +0545

    resume the app feature

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -32,76 +32,88 @@ class Home extends ConsumerWidget {
   }
 }
 
-class NoteList extends ConsumerWidget {
+class NoteList extends ConsumerStatefulWidget {
   const NoteList({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _NoteListState();
+}
+
+class _NoteListState extends ConsumerState<NoteList> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ref.read(dbStateProvider).fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final dbData = ref.watch(dbStateProvider);
-    if (dbData.notes.isEmpty) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    } else {
-      return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 7,
-        ),
-        itemCount: dbData.notes.length,
-        itemBuilder: (context, index) {
-          final note = dbData.notes[index];
-          return Container(
-            margin: const EdgeInsets.fromLTRB(13, 15, 13, 5),
-            height: MediaQuery.of(context).size.height * 0.5,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(8),
-                boxShadow: const [
-                  BoxShadow(
-                    offset: Offset(1, -2),
+    // if (dbData.notes.isEmpty) {
+    //   return const Center(
+    //     child: CircularProgressIndicator(),
+    //   );
+    // } else {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 7,
+      ),
+      itemCount: dbData.notes.length,
+      itemBuilder: (context, index) {
+        final note = dbData.notes[index];
+        return Container(
+          margin: const EdgeInsets.fromLTRB(13, 15, 13, 5),
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(1, -2),
+                  color: Colors.grey,
+                  spreadRadius: 3,
+                  blurRadius: 1,
+                )
+              ]),
+          child: Column(
+            children: [
+              Text(
+                note.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                note.content,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  note.date,
+                  style: const TextStyle(
                     color: Colors.grey,
-                    spreadRadius: 3,
-                    blurRadius: 1,
-                  )
-                ]),
-            child: Column(
-              children: [
-                Text(
-                  note.title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  note.content,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    note.date,
-                    style: const TextStyle(
-                      color: Colors.grey,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      );
-    }
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
+// }
